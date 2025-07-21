5182fbb3095776ed6a753ea23b4e1e9846ef7b82
Zacharie Rahman <68432531+noobyxviii@users.noreply.github.com>
2025-07-21 17:56:16 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "stepr.app"
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/", methods=["GET"])
+def landing():
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/privacy-policy", methods=["GET"])
+def privacypolicy():
+    return render_template("privacypolicy.html")
+
+@app.route("/download", method=["GET"])
+def download():
+    return redirect("https://apps.apple.com/us/app/stepr-walk-more-pedometer/id6748864383")
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 3000)
